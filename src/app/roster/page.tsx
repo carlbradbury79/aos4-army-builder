6ba971dd25282769f4fa28e75c6fd6798be914db
Carl Bradbury <frontside720@gmail.com>
2024-10-20 23:13:26 +0100

    [feature] redirect if no faction on roster page

--- src/app/roster/page.tsx
+++ src/app/roster/page.tsx
@@ -1,8 +1,19 @@
 "use client";
-import React from "react";
+import React, { use, useContext, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { ArmyContext } from "@/context/armyContext";
 import Army from "@/components/Army";
 
 const ArmyBuilder: React.FC = () => {
+  const { faction } = useContext(ArmyContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!faction) {
+      router.push("/");
+    }
+  }, [faction, router]);
+
   return (
     <div>
       <h1>Army Builder</h1>
