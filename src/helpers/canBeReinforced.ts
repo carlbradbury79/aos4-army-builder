063ea4643bfbd81de63e669284fc507f4bfa5a37
Carl Bradbury <frontside720@gmail.com>
2024-10-29 21:44:18 +0000

    [fix] canBeReinforced

--- src/helpers/canBeReinforced.ts
+++ src/helpers/canBeReinforced.ts
@@ -1,3 +1,4 @@
 import { Unit } from "@/types";
 
-export const canBeReinforced = (unit: Unit): boolean => unit.quantity > 1;
+export const canBeReinforced = (unit: Unit): boolean =>
+  unit.quantity > 1 && !unit.cannotBeReinforced;
